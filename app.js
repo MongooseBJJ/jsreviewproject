2f1ac89b2b1616567b2108e56a166ee69ebfce3e
MongooseBJJ <nkgwebdev@gmail.com>
2022-09-08 07:28:28 -0500

    feat/add: added appjs local data

---
+++ app.js
@@ -0,0 +1,34 @@
+//local data, normally would using Angular
+const reviews = [{
+        id: 1,
+        name: "Mai Mai Maui",
+        dominant: "Sativa",
+        img: "https://cdn.pixabay.com/photo/2017/03/16/23/19/cannabis-2150543_960_720.jpg",
+        text: "Random description text that probably doesn't mean anything to the reader anyway"
+
+    },
+    {
+        id: 2,
+        name: "Blueberry Cake",
+        dominant: "Indica",
+        img: "https://cdn.pixabay.com/photo/2017/07/18/22/22/weed-2517251_960_720.jpg",
+        text: "Random description text that probably doesn't mean anything to the reader anyway"
+
+    },
+    {
+        id: 3,
+        name: "Ice road trucker",
+        dominant: "Sativa",
+        img: "https://cdn.pixabay.com/photo/2014/09/16/19/52/cannabis-448661_960_720.jpg", // photo by pixabay user cheifyc
+        text: "Random description text that probably doesn't mean anything to the reader anyway"
+
+    },
+    {
+        id: 4,
+        name: "Star Fox",
+        dominant: "Hybrid",
+        img: "https://cdn.pixabay.com/photo/2018/11/07/19/00/bud-3801028_960_720.jpg", //photo by pixabay user David Cardinez
+        text: "Random description text that probably doesn't mean anything to the reader anyway"
+
+    },
+]
